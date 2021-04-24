160940bd77cc900e674981eb718396b9b5ada212
WilliamScottRowell <64656439+WilliamScottRowell@users.noreply.github.com>
2021-04-24 13:33:43 -0700

    Updated Movement

---
+++ GaD Week 2 Tutorial/Assets/SimplePlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayerController : MonoBehaviour
+{
+    // Variables for movement
+    public float mouseSensitivity = 100f;
+    public float speed = 12f;
+
+    public Transform playerBody;
+    public CharacterController controller;
+
+
+    // Variables for gravity
+    Vector3 velocity;
+    public float gravity = -9.81f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Turn player based on mouse movement
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        // Move player based on keyboard presses
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+
+
+        // Code for gravity
+        if(controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
